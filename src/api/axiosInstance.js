3bfd4074bc380dcb4cb9c456a0ae36a7bf3c080d
karan9639 <kvnisad18114982@gmail.com>
2025-06-06 18:24:18 +0530

    after merge code

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -4,7 +4,7 @@ import { ROUTES } from "../constants/routes";
 const API = axios.create({
   baseURL: import.meta.env.VITE_PUBLIC_API_BASE_URL, // This will use your https://Jasmine-automate-business-solutions.onrender.com/api/v1/
   withCredentials: true,
-  timeout: 30000, // 30 seconds timeout
+  timeout: 100000, // 100 seconds timeout
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
